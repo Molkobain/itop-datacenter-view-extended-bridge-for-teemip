c77b2ddabb50d2a8cc50a8d68cb0701a5b66e49d
Molkobain <lajarige.guillaume@free.fr>
2020-03-16 23:03:56 +0100

    :tada: First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Datacenter view extended ❤ IPAM for iTop
+
+## Description
+Bridge module for **Datacenter View Extended** and **IPAM for iTop** v2.3.0+ extensions. Will make sure they are fully compatible (typically, position the `ipaddress_id` field correctly).
+
+## Compatibility
+Install only if you have the following extensions on your iTop:
+* `molkobain-datacenter-view-extended/1.4.0` or newer
+* `teemip-datacenter-mgmt-adaptor/2.3.0` or newer
+
+## Installation
+* Go to the _releases_ page and download the same version number as the _Datacenter view extended_ version you have on your iTop.
+* Unzip the file and put the _molkobain-datacenter-view-extended-bridge-for-teemip_ folder under the _/extensions_ folder of your iTop
+* Run the setup, check the bridge and you are all set!
---
+++ dist/molkobain-datacenter-view-extended-bridge-for-teemip/module.molkobain-datacenter-view-extended-bridge-for-teemip.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright (c) 2015 - 2020 Molkobain.
+ *
+ * This file is part of licensed extension.
+ *
+ * Use of this extension is bound by the license you purchased. A license grants you a non-exclusive and non-transferable right to use and incorporate the item in your personal or commercial projects. There are several licenses available (see https://www.molkobain.com/usage-licenses/ for more informations)
+ */
+
+/** @noinspection PhpUnhandledExceptionInspection */
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'molkobain-datacenter-view-extended-bridge-for-teemip/1.0.0', array(
+        // Identification
+        'label' => 'Bridge - Datacenter view extended + IPAM for iTop = ❤',
+        'category' => 'business',
+
+        // Setup
+        'dependencies' => array(
+            'molkobain-datacenter-view-extended/1.4.0',
+	        'teemip-datacenter-mgmt-adaptor/2.3.0',
+        ),
+        'mandatory' => false,
+        'visible' => true,
+		//'auto_select' => 'SetupInfo::ModuleIsSelected("molkobain-datacenter-view-extended") && SetupInfo::ModuleIsSelected("teemip-datacenter-mgmt-adaptor")',
+
+        // Components
+        'datamodel' => array(
+        ),
+        'webservice' => array(
+        ),
+        'dictionary' => array(
+        ),
+        'data.struct' => array(
+        ),
+        'data.sample' => array(
+        ),
+
+        // Documentation
+        'doc.manual_setup' => '',
+        'doc.more_information' => '',
+
+        // Default settings
+        'settings' => array(
+        ),
+	)
+);
